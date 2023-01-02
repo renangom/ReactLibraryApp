95136559a1ccf1ce1a03b4d0d6a84559cc4017cc
Renan <renan23eduardo@gmail.com>
2023-01-02 09:06:53 -0300

    Create navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello World</h1>
+      <Navbar />
     </div>
   );
 }
