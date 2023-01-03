e88a82152eb5fc8f11aa8a8f34188c654279f9fa
Renan <renan23eduardo@gmail.com>
2023-01-03 07:58:30 -0300

    Create carousel component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import "./App.css"
 import ExploreTopBooks from './components/ExploreTopBooks';
+import Carousel from './components/Carousel';
 
 function App() {
   return (
     <div>
       <Navbar />
       <ExploreTopBooks />
+      <Carousel />
     </div>
   );
 }
