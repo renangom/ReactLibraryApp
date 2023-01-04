b0e7eb15bc1abc7963ac06f0764c805ffe68c8fc
Renan <renan23eduardo@gmail.com>
2023-01-04 14:07:39 -0300

    Fix heros component

--- src/components/Heros/index.tsx
+++ src/components/Heros/index.tsx
@@ -6,9 +6,9 @@ export default function Heros() {
         <div className='d-none d-lg-block'>
             <div className='row g-0 mt-5'>
                 <div className='col-sm-6 col-md-6'>
-                    <div className='col-image-left'>
-                    </div>
-                    <div className='col-4 col-md-4 container d-flex justify-content-center align-items-center'>
+                    <div className='col-image-left'></div>
+                </div>
+                <div className='col-4 col-md-4 container d-flex justify-content-center align-items-center'>
                         <div className='ml-2'>
                             <h1>What have you been reading?</h1>
                             <p className='lead'>
@@ -19,7 +19,6 @@ export default function Heros() {
                             <a className='btn main-color btn-lg text-white' href='#'>Sign Up</a>
                         </div>
                     </div>
-                </div>
                 <div className='row g-0'>
                     <div className='col-4 col-md-4 container d-flex justify-content-center align-items-center'>
                         <div className='ml-2'>
@@ -46,7 +45,7 @@ export default function Heros() {
             <div className='d-lg-none'>
                 <div className="container">
                     <div className="m-2">
-                        <div className="col-image-lef"></div>
+                        <div className="col-image-left"></div>
                         <div className="mt-2">
                             <h1>What have you been reading?</h1>
                             <p className='lead'>
